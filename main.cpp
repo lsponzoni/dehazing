4ab1927202e3986c0d538780a50f215eb7d7dada
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2011-12-17 16:46:29 -0200

    Moved estimateTransmissionMultiAlbedo to transmission.h

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     tmatrix *im = normQImage(image);
 
     dtriple airlight(0.8, 0.8, 0.9);
-    dmatrix *t = estimateTransmission(im, airlight);
+    dmatrix *t = estimateTransmissionSingleAlbedo(im, airlight);
 
     dmToQImage(t).save("trans" + inFile);
 
@@ -43,107 +43,3 @@ int main(int argc, char *argv[])
 
     return 0;//a.exec();
 }
-
-//*************************************************************************
-// After better redo
-// Multi albedo methods
-// TODO refactor code
-//*************************************************************************
-// both y and x are pixels
-//
-dmatrix *estimateTransmissionMultiAlbedo (tmatrix *image, dtriple airlight)
-{
-    double normA = norm(airlight);
-
-    const double WR = -0.299, WB = -0.114, WG = -0.587, CWR = 1 + WR, CWB = 1 + WG;
-
-    // 1. compute naive haze free In = I - A*Ia/ normA
-    tmatrix In(image->w, image->h);
-    dmatrix Ia(image->w, image->h);
-    for (int i=0; i<image->h; i++) {
-        for (int j=0; j<image->w; j++) {
-            dtriple imP = image->pos(i,j);
-
-            double dotProd = imP.fst * airlight.fst
-                            + imP.snd * airlight.snd
-                            + imP.trd * airlight.trd;
-
-            // Calculate Ia
-            double Ia_value = dotProd / normA;
-            Ia.pos(i,j) = Ia_value;
-
-            // Calculate In
-            double r = imP.fst - airlight.fst * Ia_value / normA;
-            double g = imP.snd - airlight.snd * Ia_value / normA;
-            double b = imP.trd - airlight.trd * Ia_value / normA;
-
-            In.pos(i,j) = dtriple(r,g,b);
-        }
-    }
-
-    // 2. extract a single color channel o(x) = 1/tan( (Inaive .* U)/Inaive .* V)
-    // ignoring shading
-    dmatrix o(image->w, image->h);
-    for (int i=0; i<image->h; i++) {
-        for (int j=0; j<image->w; j++) {
-            dtriple imP = In.pos(i,j);
-
-            double u = imP.fst * WR
-                      + imP.snd * WG
-                      + imP.trd * CWB;
-
-            double b = imP.fst * CWR
-                      + imP.snd * WG
-                      + imP.trd * WB;
-
-            o.pos(i,j) = 1/tan(u / b );
-        }
-    }
-
-    // Calculate Ir
-    dmatrix Ir(image->w, image->h);
-    for (int i=0; i<image->h; i++) {
-        for (int j=0; j<image->w; j++) {
-            double imNorm = norm(image->pos(i,j));
-            double iaP = Ia.pos(i,j);
-            Ir.pos(i,j) = sqrt( sqr(imNorm) - sqr(iaP) );
-        }
-    }
-
-    // Calculate h
-    dmatrix h(image->w, image->h);
-    for (int i=0; i<image->h; i++) {
-        for (int j=0; j<image->w; j++) {
-            double iaP = Ia.pos(i,j);
-            double irP = Ir.pos(i,j);
-            h.pos(i,j) = (normA - iaP) / irP;
-        }
-    }
-    // TODO the methods
-    dmatrix n(image->w, image->h);
-    for (int i=0; i<image->h; i++) {
-        for (int j=0; j<image->w; j++) {
-            n.pos(i,j) = w_covar(i, j, &Ia, &h, &o)/ w_covar(i, j, &Ir, &h, &o);
-        }
-    }
-
-    // Calculate the transmission (commented: shading)
-    dmatrix *t = new dmatrix(h.w, h.h);
-    //dmatrix *l = new dmatrix(h.w, h.h);
-    for (int i=0; i<image->h; i++) {
-        for (int j=0; j<image->w; j++) {
-            double iaP = Ia.pos(i,j);
-            double irP = Ir.pos(i,j);
-            double nP = n.pos(i,j);
-
-            double tP = 1 - (iaP - nP * irP) / normA;
-
-            t->pos(i,j) = tP;
-            //l->pos(i,j) = irP / tP;
-        }
-    }
-
-    // this should still be refined by a markov chain
-    return t;
-}
-
--- transmission.cpp
+++ transmission.cpp
@@ -65,6 +65,103 @@ dmatrix *estimateTransmission (tmatrix *image, dtriple airlight)
     return t;
 }
 
+dmatrix *estimateTransmissionMultiAlbedo (tmatrix *image, dtriple airlight)
+{
+    double normA = norm(airlight);
+
+    const double WR = -0.299, WB = -0.114, WG = -0.587, CWR = 1 + WR, CWB = 1 + WG;
+
+    // 1. compute naive haze free In = I - A*Ia/ normA
+    tmatrix In(image->w, image->h);
+    dmatrix Ia(image->w, image->h);
+    for (int i=0; i<image->h; i++) {
+        for (int j=0; j<image->w; j++) {
+            dtriple imP = image->pos(i,j);
+
+            double dotProd = imP.fst * airlight.fst
+                            + imP.snd * airlight.snd
+                            + imP.trd * airlight.trd;
+
+            // Calculate Ia
+            double Ia_value = dotProd / normA;
+            Ia.pos(i,j) = Ia_value;
+
+            // Calculate In
+            double r = imP.fst - airlight.fst * Ia_value / normA;
+            double g = imP.snd - airlight.snd * Ia_value / normA;
+            double b = imP.trd - airlight.trd * Ia_value / normA;
+
+            In.pos(i,j) = dtriple(r,g,b);
+        }
+    }
+
+    // 2. extract a single color channel o(x) = 1/tan( (Inaive .* U)/Inaive .* V)
+    // ignoring shading
+    dmatrix o(image->w, image->h);
+    for (int i=0; i<image->h; i++) {
+        for (int j=0; j<image->w; j++) {
+            dtriple imP = In.pos(i,j);
+
+            double u = imP.fst * WR
+                      + imP.snd * WG
+                      + imP.trd * CWB;
+
+            double b = imP.fst * CWR
+                      + imP.snd * WG
+                      + imP.trd * WB;
+
+            o.pos(i,j) = 1/tan(u / b );
+        }
+    }
+
+    // Calculate Ir
+    dmatrix Ir(image->w, image->h);
+    for (int i=0; i<image->h; i++) {
+        for (int j=0; j<image->w; j++) {
+            double imNorm = norm(image->pos(i,j));
+            double iaP = Ia.pos(i,j);
+            Ir.pos(i,j) = sqrt( sqr(imNorm) - sqr(iaP) );
+        }
+    }
+
+    // Calculate h
+    dmatrix h(image->w, image->h);
+    for (int i=0; i<image->h; i++) {
+        for (int j=0; j<image->w; j++) {
+            double iaP = Ia.pos(i,j);
+            double irP = Ir.pos(i,j);
+            h.pos(i,j) = (normA - iaP) / irP;
+        }
+    }
+    // TODO the methods
+    dmatrix n(image->w, image->h);
+    for (int i=0; i<image->h; i++) {
+        for (int j=0; j<image->w; j++) {
+            n.pos(i,j) = w_covar(i, j, &Ia, &h, &o)/ w_covar(i, j, &Ir, &h, &o);
+        }
+    }
+
+    // Calculate the transmission (commented: shading)
+    dmatrix *t = new dmatrix(h.w, h.h);
+    //dmatrix *l = new dmatrix(h.w, h.h);
+    for (int i=0; i<image->h; i++) {
+        for (int j=0; j<image->w; j++) {
+            double iaP = Ia.pos(i,j);
+            double irP = Ir.pos(i,j);
+            double nP = n.pos(i,j);
+
+            double tP = 1 - (iaP - nP * irP) / normA;
+
+            t->pos(i,j) = tP;
+            //l->pos(i,j) = irP / tP;
+        }
+    }
+
+    // this should still be refined by a markov chain
+    return t;
+}
+
+
 tmatrix *removeAirlight(tmatrix *image, dmatrix *transmission, dtriple airlight)
 {
     tmatrix *result = new tmatrix(image->w, image->h);
